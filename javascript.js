7d1b1df9d39a8749d07133e025413d0eceaa9bc3
janceboy <janceb0yyy@gmail.com>
2022-05-04 21:56:04 +0300

    added button for canvas

--- javascript.js
+++ javascript.js
@@ -10,14 +10,33 @@ function addDivs() {
 }    
 
 
-for (let i = 0; i < 256; i++) {
+
+
+
+const btn = document.querySelector('.new')
+
+
+btn.addEventListener('click', function() {
+    let styleBox = document.querySelector('.container')
+    styleBox.replaceChildren()
+    let gridSize;
+
+    gridSize = +prompt('How big of a grid do you want. max 100')
+    // let setSize = 'repeat(gridSize, 20px)'
+    styleBox.style.gridTemplateColumns = 'repeat(16, 20px)';
+
+    let squareSize = gridSize * gridSize
+
+    for (let i = 0; i < squareSize; i++) {
+
         addDivs()
     }
 
-const addHover = document.querySelectorAll('.container > div')
+    const addHover = document.querySelectorAll('.container > div')
 
-for (let i = 0; i < addHover.length; i++){
-    addHover[i].addEventListener("mouseenter", function() {
-        event.target.style.backgroundColor = "black";
-    })
-}
+    for (let i = 0; i < addHover.length; i++){
+        addHover[i].addEventListener("mouseenter", function(event) {
+            event.target.style.backgroundColor = "black";
+        })
+    }
+})
